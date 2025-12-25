7fc4934ef7f619c3b8efe04e195d826b1d178528
ashreekar <ashreekar767@gmail.com>
2025-12-25 13:07:01 +0530

    add not found styling

--- src/component/NotFound.tsx
+++ src/component/NotFound.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#F7F8F9]">
+      <h1 className="text-4xl font-bold text-[#1D2226] mb-4">404</h1>
+      <p className="text-lg text-gray-600 mb-6">Oops! Page not found.</p>
+      <Link
+        to="/"
+        className="px-4 py-2 bg-[#6C25FF] text-white rounded-md font-semibold"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
