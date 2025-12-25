1947bf5805c42f2e700b3f3689560a86874bc538
ashreekar <ashreekar767@gmail.com>
2025-12-25 11:43:13 +0530

    add welcome page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen flex justify-center bg-purple-50">
+      <div className="w-full max-w-93.75 bg-[#F7F8F9] border-4 border-gray-100">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/page/WelcomePage.tsx
+++ src/page/WelcomePage.tsx
@@ -1,8 +1,33 @@
+import { Link } from "react-router-dom";
 
 function WelcomePage() {
   return (
-    <div>WelcomePage</div>
-  )
+    <div className="min-h-screen flex flex-col justify-end px-5 pb-10">
+      <h1 className="text-[28px] font-medium leading-4.25 text-[#1D2226]">
+        Welcome to PopX
+      </h1>
+
+      <p className="mt-4 text-[18px] leading-6.5 text-[#1D2226] opacity-60 pr-16">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+
+      <Link
+        to="/create"
+        className="mt-8 h-11.5 bg-[#6C25FF] text-white font-semibold rounded-md
+                   flex items-center justify-center"
+      >
+        Create Account
+      </Link>
+
+      <Link
+        to="/login"
+        className="mt-3 h-11.5 bg-[#6C25FF4B] font-semibold rounded-md
+                   flex items-center justify-center"
+      >
+        Already Registered? Login
+      </Link>
+    </div>
+  );
 }
 
-export default WelcomePage
+export default WelcomePage;
