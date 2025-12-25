e2e76149f3bb0c9a2189b6bc0c9d01fcba0080dd
ashreekar <ashreekar767@gmail.com>
2025-12-25 11:19:33 +0530

    add: routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+
+import App from './App'
+import NotFound from './component/NotFound'
+import WelcomePage from './page/WelcomePage'
+import Login from './page/Login'
+import CreateAccount from './page/CreateAccount'
+import Dashboard from './page/Dashboard'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "",
+        element: <WelcomePage />,
+      },
+      {
+        path: "you",
+        element: <Dashboard />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "create",
+        element: <CreateAccount />,
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
